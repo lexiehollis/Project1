165fb321e443e55a46e2dd8423127a8eecc976be
Jessica Hollis <jessica.hollis@revature.net>
2022-02-11 07:35:07 -0500

    FRIDAY MORNING

--- src/main/scala/Scenario6.scala
+++ src/main/scala/Scenario6.scala
@@ -4,20 +4,22 @@ object Scenario6 {
 
   def scenario6(spark: SparkSession): Unit = {
 
+    spark.sql("drop table Milkproducts")
+
     //spark.sql("Create table MilkAdd (animal string, plant string")
+
+
+   // Spark.sql("INSERT INTO MildSELECT * FROM branches WHERE nameDrinkBran LIKE '%capp%', '%LATTE%', '%MOCHA%'")
 
     //spark.sql("INSERT INTO MilkAdd  nameDrinkCon FROM consumers WHERE nameDrinkCon INCLUDES??
 
-     // all of the drinks that i think have milk; have to distinguihs by name??
+    // all of the drinks that i think have milk; have to distinguihs by name??
 
     //capp, latte, mocha, lite???(lowball data? any additional milkadd drinks can be informative; conservative approcah))
 
-   //spark.sql("Create table Milkproducts (nameDrinkBran String, branchNum String) row format delimited fields terminated by ','")
-
-    spark.sql("LOAD DATA LOCAL INPATH 'input/Bev_BranchA.txt' INTO TABLE Milkproducts")
-    spark.sql("LOAD DATA LOCAL INPATH 'input/Bev_BranchB.txt' INTO TABLE Milkproducts")
-    spark.sql("LOAD DATA LOCAL INPATH 'input/Bev_BranchC.txt' INTO TABLE Milkproducts")
-    spark.sql("LOAD DATA LOCAL INPATH 'input/Bev_BranchC.txt' INTO TABLE Milkproducts"))
-
+    //spark.sql("Create table Milkproducts (nameDrinkBran String, branchNum String) row format delimited fields terminated by ','")
+    //spark.sql("LOAD DATA LOCAL INPATH 'input/Bev_BranchA.txt' INTO TABLE Milkproducts")
+    //spark.sql("LOAD DATA LOCAL INPATH 'input/Bev_BranchB.txt' INTO TABLE Milkproducts")
+    //spark.sql("LOAD DATA LOCAL INPATH 'input/Bev_BranchC.txt' INTO TABLE Milkproducts")
   }
 }
